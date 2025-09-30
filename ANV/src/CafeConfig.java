c93678263f60e8af54dc7ca8f30f067f2af9dd75
rutaji <jiri.ruta@tul.cz>
2025-09-30 18:35:55 +0200

    added latex

---
+++ ANV/src/CafeConfig.java
@@ -0,0 +1,22 @@
+public class CafeConfig {
+    private CafeConfig() {
+    }
+    private static CafeConfig Instance = null;
+    private String CafeName = "default name";
+    public String GetCafeName(){return CafeName;}
+    public Boolean SetCafeName(String Value) //thread unsafe
+    {
+        if(Value == null){return false;}
+        CafeName = Value;
+        return true;
+    }
+
+    public static CafeConfig getInstance() //thread unsafe
+    {
+        if(Instance == null){
+            Instance = new CafeConfig();
+        }
+        return Instance;
+    }
+
+}
